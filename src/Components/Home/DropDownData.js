509f807499e3c2e3f230a962d589c4c89a049647
Akinleye Paul <paulakinleye@gmail.com>
2023-03-21 19:42:02 +0100

    made some changes

--- src/Components/Home/DropDownData.js
+++ src/Components/Home/DropDownData.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
-import * as RiIcons from 'react-icons/ri';
+
 
 export const DropDownData = [
   {
     title: 'Products',
     path: null,
     iconClosed: <i className='bx bx-chevron-down ' ></i>,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
+    iconOpened: <i className='bx bx-chevron-up ' ></i>,
     subNav: [
       {
         // title: 'Multiple Loan offers',
         // path: '/LoanOffers',
         // cName: 'sub-nav',
         // details: 'Get  instant loan disbursed to your account under 24 hrs of application.',
-        
+
       }
     ]
 
@@ -26,18 +25,18 @@ export const DropDownData = [
     path: null,
     icon: <IoIcons.IoIosPaper />,
     iconClosed: <i className='bx bx-chevron-down ' ></i>,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
+    iconOpened: <i className='bx bx-chevron-up ' ></i>,
 
     subNav: [
       {
         title: 'Multiple Loan offers',
         path: '/LoanOffers',
-      
+
       },
       {
         title: 'Credit Score',
         path: '/ScoreRating',
-       
+
       },
       {
         title: 'Free Transfer',
@@ -53,10 +52,7 @@ export const DropDownData = [
         title: 'API Integrations',
         path: '/PartnersIntegrations',
       },
-      {
-        title: 'API Integrations',
-       
-      },
+
     ]
   },
   {
@@ -64,28 +60,28 @@ export const DropDownData = [
     path: null,
     icon: <FaIcons.FaEnvelopeOpenText />,
     iconClosed: <i className='bx bx-chevron-down ' ></i>,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
+    iconOpened: <i className='bx bx-chevron-up ' ></i>,
 
     subNav: [
       {
         title: 'Latest Articles  and Research',
         path: '/',
-       
+
       },
       {
         title: 'Customer Hub',
         path: '/LendingPartners',
-       
+
       },
       {
         title: 'FAQs',
         path: '/FAQs',
         // cName: 'sub-nav',
-     
+
       }, {
         title: 'API Documentation',
         path: '/',
-    
+
       },
     ]
   },
@@ -94,7 +90,7 @@ export const DropDownData = [
     path: null,
     icon: <FaIcons.FaEnvelopeOpenText />,
     iconClosed: <i className='bx bx-chevron-down ' ></i>,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
+    iconOpened: <i className='bx bx-chevron-up ' ></i>,
 
     subNav: [
       {
@@ -104,7 +100,7 @@ export const DropDownData = [
       {
         title: 'Contact us',
         path: '/contactus',
-  
+
       },
       {
         title: 'Career',
