42c23d3029325703457b6f34a1dea33298ae158f
Paul Akinleye <paulakinleye@gmail.com>
2023-06-16 01:07:09 -0700

    chore: changed colors using variables

--- src/Components/Home/DropDown.js
+++ src/Components/Home/DropDown.js
@@ -1,28 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import logoImg from '../Home/Images/Credbevy Logo.svg'
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import logoImg from "../Home/Images/Credbevy Logo.svg";
 // import { device } from './Device';
-import { DropDownData } from './DropDownData'
-import DropDownSubMenu from './DropDownSubMenu';
-import Button from './Utilities/Button';
+import { DropDownData } from "./DropDownData";
+import DropDownSubMenu from "./DropDownSubMenu";
+import Button from "./Utilities/Button";
 // import { Dropdown } from 'rsuite';
 
-
-
-
 const Nav = styled.div`
-  background-color: #fef9fe;  
+  background-color: var(--pink);
   height: 65px;
   display: flex;
   justify-content: center;
   align-items: center;
   position: relative;
   top: 2.5rem;
-  width:100%;
-  gap:16rem;
+  width: 100%;
+  gap: 16rem;
 `;
 
 const NavIcon = styled(Link)`
@@ -34,158 +31,162 @@ const NavIcon = styled(Link)`
 `;
 
 const NavImage = styled(Link)`
-width: 2rem;
-height: 2rem;
+  width: 2rem;
+  height: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-
-
-
-
 const NavIcon2 = styled(Link)`
   font-size: 1rem;
   height: 2rem;
 
-display:flex;
-flex-direction:center;
-align-items:center;
+  display: flex;
+  flex-direction: center;
+  align-items: center;
 `;
 
 const NavLogo = styled(Link)`
-width: 2rem;
-height: 2rem;
-
+  width: 2rem;
+  height: 2rem;
 `;
 
-
 const NavItems = styled.div`
-    background-color: white;
-display:flex;
-flex-direction:row;
-justify-content:center;
-gap:16rem;
-margin-bottom:4rem;
-
-`
-
+  background-color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 16rem;
+  margin-bottom: 4rem;
+`;
 
 const DropDownNav = styled.nav`
-background:white;
-padding-top:1rem;
-color: black; 
-width: 377px;
-height: 417px; 
-justify-content:center;
-position: absolute;
-// left:0px;
-top: ${({ dropDown }) => (dropDown ? '-1.5rem' : '-1000%')};
-transition:1ms;
-z-index:100;
-// box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.25);
-border-radius: 8px;
-
-`
+  background: white;
+  padding-top: 1rem;
+  color: black;
+  width: 377px;
+  height: 417px;
+  justify-content: center;
+  position: absolute;
+  // left:0px;
+  top: ${({ dropDown }) => (dropDown ? "-1.5rem" : "-1000%")};
+  transition: 1ms;
+  z-index: 100;
+  // box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+`;
 const NavButton = styled.div`
-display:flex;
-flex-direction:row;
-justify-content:center;
-margin-top:2rem;
-// background:white;
-
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 2rem;
+  // background:white;
+`;
 const DropDownWrap = styled.div`
-// width:100%;
-background:white;
-display:flex;
-flex-direction:column;
-gap:0.4rem;
-border-radius: 8px;
-// position:absolute;
-z-index:10;
-`
+  // width:100%;
+  background: white;
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  border-radius: 8px;
+  // position:absolute;
+  z-index: 10;
+`;
 const OverLay = styled.div`
-  background: #8003CD;
+  background: var(--purple);
   opacity: 0.05;
-  position:absolute;
-  height:80vh;
-  top:-40px;
-  width:100%;
-  z-index:10;
-`
+  position: absolute;
+  height: 80vh;
+  top: -40px;
+  width: 100%;
+  z-index: 10;
+`;
 function DropDown() {
-
   const sizeStyle = {
-    width: '345px',
-  }
+    width: "345px",
+  };
 
-  const [dropDown, setDropDown] = useState(false)
-  const showDropDown = () => setDropDown(!dropDown)
+  const [dropDown, setDropDown] = useState(false);
+  const showDropDown = () => setDropDown(!dropDown);
 
-  const [overlay, setOverLay] = useState(false)
-  const showOverLay = () => setOverLay(!overlay)
+  const [overlay, setOverLay] = useState(false);
+  const showOverLay = () => setOverLay(!overlay);
 
-  const showMenuReff = useRef(null)
+  const showMenuReff = useRef(null);
 
   useEffect(() => {
-
     const pageClickEvent = (e) => {
-      if (showMenuReff.current !== null && !showMenuReff.current.contains(e.target)) {
+      if (
+        showMenuReff.current !== null &&
+        !showMenuReff.current.contains(e.target)
+      ) {
         setDropDown(!dropDown);
       }
-    }
+    };
     if (dropDown) {
-      window.addEventListener('click', pageClickEvent)
+      window.addEventListener("click", pageClickEvent);
     }
     return () => {
-      window.removeEventListener('click', pageClickEvent)
-    }
-  }, [dropDown])
+      window.removeEventListener("click", pageClickEvent);
+    };
+  }, [dropDown]);
 
   return (
     <>
       <Nav>
         <NavImage>
-          <img className='mobile_navbar__image' src={logoImg} alt='logo-img'></img>
+          <img
+            className="mobile_navbar__image"
+            src={logoImg}
+            alt="logo-img"
+          ></img>
         </NavImage>
-        <NavIcon onClick={() => {
-          showDropDown();
-          showOverLay();
-        }} to='#'>
+        <NavIcon
+          onClick={() => {
+            showDropDown();
+            showOverLay();
+          }}
+          to="#"
+        >
           {dropDown ? <AiIcons.AiOutlineClose /> : <FaIcons.FaBars />}
         </NavIcon>
         {overlay ? <OverLay /> : null}
         <DropDownNav dropDown={dropDown}>
           <NavItems>
             <NavLogo>
-              <img className='mobile_navbar__image' src={logoImg} alt='logo-img'></img>
+              <img
+                className="mobile_navbar__image"
+                src={logoImg}
+                alt="logo-img"
+              ></img>
             </NavLogo>
-            <NavIcon2 onClick={() => {
-              showDropDown();
-              showOverLay();
-            }} to='#'>
+            <NavIcon2
+              onClick={() => {
+                showDropDown();
+                showOverLay();
+              }}
+              to="#"
+            >
               {dropDown ? <AiIcons.AiOutlineClose /> : <FaIcons.FaBars />}
             </NavIcon2>
           </NavItems>
-          <DropDownWrap   >
+          <DropDownWrap>
             {DropDownData.map((item, index) => {
-              return <DropDownSubMenu item={item} key={index} />
+              return <DropDownSubMenu item={item} key={index} />;
             })}
             <NavButton>
               <Button
-                className='button__container'
+                className="button__container"
                 buttonStyle={sizeStyle}
-                label={'Get Started'} />
+                label={"Get Started"}
+              />
             </NavButton>
           </DropDownWrap>
-
         </DropDownNav>
       </Nav>
-
     </>
-  )
+  );
 }
 
-export default DropDown
+export default DropDown;
--- src/Components/Home/DropDownSubMenu.js
+++ src/Components/Home/DropDownSubMenu.js
@@ -1,138 +1,137 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from "react";
 // import { IoMdHeartEmpty } from 'react-icons/io'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const DropDownLink = styled(Link)`
-display:flex;
-flex-direction:row;
-background:white;
-justify-content:center;
-align-items:center;
-text-decoration:none;
-margin-bottom:1.1rem;
-gap:15rem;
-// width: 63px;
-height: 21px;
-font-family: 'Manrope';
-font-style: normal;
-font-weight: 700;
-font-size: 14px;
-line-height: 150%;
-color: #333333;
-&:hover {
-  cursor:pointer;
-}
+  display: flex;
+  flex-direction: row;
+  background: white;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  margin-bottom: 1.1rem;
+  gap: 15rem;
+  // width: 63px;
+  height: 21px;
+  font-family: "Manrope";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 150%;
+  color: var(--gray);
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const SideBarLabel = styled.span`
-// height: 19px;
-background:white;
+  // height: 19px;
+  background: white;
 
-font-family: 'Manrope';
-font-style: normal;
-font-weight: 500;
-font-size: 14px;
-line-height: 117%;
-/* or 16px */
-/* Grey 1 */
-color: #8A8B9F;
-padding-left:2rem;
-`
-
+  font-family: "Manrope";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 117%;
+  /* or 16px */
+  /* Grey 1 */
+  color: #8a8b9f;
+  padding-left: 2rem;
+`;
 
 const DropdownTitle = styled.div`
-width:3.5rem;
-font-family: 'Manrope';
-font-style: normal;
-font-weight: 700;
-font-size: 14px;
-line-height: 150%;
-color: #333333;
-background:white;
-
-`
+  width: 3.5rem;
+  font-family: "Manrope";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 150%;
+  color: var(--gray);
+  background: white;
+`;
 
 const DropdownLink = styled(Link)`
-background:white;
-// height:2rem;
-display:flex;
-flex-direction:column;
-text-decoration:none;
-color:white;
-font-size:18px;
-margin-bottom:1rem;
-// border:none;
-border-radius: 0px 0px 8px 8px;
-
-
-`
+  background: white;
+  // height:2rem;
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+  color: white;
+  font-size: 18px;
+  margin-bottom: 1rem;
+  // border:none;
+  border-radius: 0px 0px 8px 8px;
+`;
 const SideBarLabel2 = styled.span`
-// height: 19px;
-background:white;
+  // height: 19px;
+  background: white;
 
-font-family: 'Manrope';
-font-style: normal;
-font-weight: 700;
-font-size: 14px;
-line-height: 150%;
-/* identical to box height, or 21px */
-// margin-top:1rem;
-/* Primary */
-color: #8003CD;
-padding: 0rem 0rem 0rem 2rem;
-`
-
+  font-family: "Manrope";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 150%;
+  /* identical to box height, or 21px */
+  // margin-top:1rem;
+  /* Primary */
+  color: var(--purple);
+  padding: 0rem 0rem 0rem 2rem;
+`;
 
 function DropDownSubMenu({ item }) {
-  const [subnav, setSubNav] = useState(false)
-  const showSubnav = () => setSubNav(!subnav)
+  const [subnav, setSubNav] = useState(false);
+  const showSubnav = () => setSubNav(!subnav);
 
-
-  const showMenuRef = useRef(null)
+  const showMenuRef = useRef(null);
 
   useEffect(() => {
-
     const pageClickEvent = (e) => {
-      if (showMenuRef.current !== null && !showMenuRef.current.contains(e.target)) {
+      if (
+        showMenuRef.current !== null &&
+        !showMenuRef.current.contains(e.target)
+      ) {
         setSubNav(!subnav);
       }
-    }
+    };
     if (subnav) {
-      window.addEventListener('click', pageClickEvent)
+      window.addEventListener("click", pageClickEvent);
     }
     return () => {
-      window.removeEventListener('click', pageClickEvent)
-    }
-  }, [subnav])
+      window.removeEventListener("click", pageClickEvent);
+    };
+  }, [subnav]);
 
   return (
     <>
-      <DropDownLink to={item.path}
+      <DropDownLink
+        to={item.path}
         ref={showMenuRef}
-        onClick={item.subNav && showSubnav}>
+        onClick={item.subNav && showSubnav}
+      >
         <div>
-          <DropdownTitle>  {item.title} </DropdownTitle>
+          <DropdownTitle> {item.title} </DropdownTitle>
         </div>
 
         <div>
-          {item.subNav && subnav ? item.iconOpened : item.subNav ? item.iconClosed : null}
+          {item.subNav && subnav
+            ? item.iconOpened
+            : item.subNav
+            ? item.iconClosed
+            : null}
         </div>
       </DropDownLink>
-      {subnav && item.subNav.map((item, index) => {
-        return (
-          <DropdownLink to={item.path} key={index}>
-            <SideBarLabel>{item.title}</SideBarLabel>
-            <SideBarLabel2>{item.topic}</SideBarLabel2>
-          </DropdownLink>
-        )
-      })}
+      {subnav &&
+        item.subNav.map((item, index) => {
+          return (
+            <DropdownLink to={item.path} key={index}>
+              <SideBarLabel>{item.title}</SideBarLabel>
+              <SideBarLabel2>{item.topic}</SideBarLabel2>
+            </DropdownLink>
+          );
+        })}
     </>
-  )
-
-
-
+  );
 }
 
-export default DropDownSubMenu
+export default DropDownSubMenu;
