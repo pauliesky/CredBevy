a9789baa31045df472aaa3014a89ae76daae04de
Paul Akinleye <paulakinleye@gmail.com>
2023-09-24 02:11:54 +0100

    chore:implemented contact us api

--- src/Components/Contact/ContactUs.js
+++ src/Components/Contact/ContactUs.js
@@ -1,71 +1,148 @@
-import React from 'react'
-import './ContactUs.css'
-import Button from '../Home/Utilities/Button'
+import React from "react";
+import "./ContactUs.css";
+import Button from "../Home/Utilities/Button";
+import { useState } from "react";
 
 const ContactUs = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [timeStamp, setTimeStamp] = useState();
 
   const buttonStyleSize = {
-    width: '159px',
-    height: '55px',
-  }
+    width: "159px",
+    height: "55px",
+  };
 
   const buttonTextSize = {
-    width: '110px',
-    height: '21px',
+    width: "110px",
+    height: "21px",
+  };
+
+  function Submit(e) {
+    const formEle = document.querySelector(".contactUs__form");
+    e.preventDefault();
+    console.log("Submitted");
+    setEmail("");
+    setMessage("");
+    setName("");
+
+    // console.log(timestamp());
+    const formDatab = new FormData(formEle);
+    fetch(
+      "https://script.google.com/macros/s/AKfycbwAkb9MozirPX627i200C7twGmvBR_sBmbrHWmwzmZWK1lm6y7hStcYBSlPtLLNObFD/exec",
+      {
+        method: "POST",
+        body: formDatab,
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  // const style = {
-  //   height: '175px',
-  //   width: '466px',
-  //   padding: '20px',
-  //   backgroundColor: '#f5f5f5',
-  //   border: 'none',
-  //   resize: 'none',
-  //   borderRadius: '8px',
-  //   marginBottom: '6rem',
-  // }
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+    const date = new Date().toLocaleDateString("en-EN", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+    setTimeStamp(date);
+  };
+
+  // const handleTimestampChange = () => {
+
+  // };
+
   return (
     <>
-      <div className='contactUs__wrapper'>
-        <div className='contactUs__head'>
+      <div className="contactUs__wrapper">
+        <div className="contactUs__head">
           <h5>Contact us</h5>
         </div>
-        <div className='contactUs__title' > We'll love to hear from you</div>
-        <form className='contactUs__form'>
+        <div className="contactUs__title"> We'll love to hear from you</div>
+        <form onSubmit={(e) => Submit(e)} className="contactUs__form">
           <label>Name</label>
           <input
+            value={name}
+            onChange={handleNameChange}
+            name="Name"
             className="input__size"
-            type='text'
+            type="text"
             required
           />
           <label>Email</label>
           <input
+            onChange={handleChange}
+            value={email}
+            name="Email"
             className="input__size"
-            type='email'
+            type="email"
             required
           />
           <label>Your Message</label>
           <textarea
-          className="textarea"
-          // style={style}
-          // type='textarea'
-          // name='message'
-          // required
+            required
+            onChange={handleMessageChange}
+            value={message}
+            name="Message"
+            type="text"
+            className="textarea"
           ></textarea>
-          <div className='contactUs__btn-wrapper' >
+          <input
+            onChange={handleChange}
+            value={timeStamp}
+            name="Timestamp"
+            type="Timestamp"
+            hidden
+          />
+          <div className="contactUs__btn-wrapper">
             <Button
-              className='contactus-button__container'
+              type="submit"
+              value="Submit"
+              className="contactus-button__container"
               buttonTextStyle={buttonTextSize}
               buttonStyle={buttonStyleSize}
-              label={'Send Message'} />
+              label={"Send Message"}
+            />
           </div>
-
         </form>
 
+        <div>
+          <p>Message successfully sent</p>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13.4099 12L19.7099 5.71C19.8982 5.5217 20.004 5.2663 20.004 5C20.004 4.7337 19.8982 4.47831 19.7099 4.29C19.5216 4.1017 19.2662 3.99591 18.9999 3.99591C18.7336 3.99591 18.4782 4.1017 18.2899 4.29L11.9999 10.59L5.70994 4.29C5.52164 4.1017 5.26624 3.99591 4.99994 3.99591C4.73364 3.99591 4.47824 4.1017 4.28994 4.29C4.10164 4.47831 3.99585 4.7337 3.99585 5C3.99585 5.2663 4.10164 5.5217 4.28994 5.71L10.5899 12L4.28994 18.29C4.19621 18.383 4.12182 18.4936 4.07105 18.6154C4.02028 18.7373 3.99414 18.868 3.99414 19C3.99414 19.132 4.02028 19.2627 4.07105 19.3846C4.12182 19.5064 4.19621 19.617 4.28994 19.71C4.3829 19.8037 4.4935 19.8781 4.61536 19.9289C4.73722 19.9797 4.86793 20.0058 4.99994 20.0058C5.13195 20.0058 5.26266 19.9797 5.38452 19.9289C5.50638 19.8781 5.61698 19.8037 5.70994 19.71L11.9999 13.41L18.2899 19.71C18.3829 19.8037 18.4935 19.8781 18.6154 19.9289C18.7372 19.9797 18.8679 20.0058 18.9999 20.0058C19.132 20.0058 19.2627 19.9797 19.3845 19.9289C19.5064 19.8781 19.617 19.8037 19.7099 19.71C19.8037 19.617 19.8781 19.5064 19.9288 19.3846C19.9796 19.2627 20.0057 19.132 20.0057 19C20.0057 18.868 19.9796 18.7373 19.9288 18.6154C19.8781 18.4936 19.8037 18.383 19.7099 18.29L13.4099 12Z"
+              fill="white"
+            />
+          </svg>
+        </div>
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
